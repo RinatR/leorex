24c71ddb2019aef9cd8a33778abe283888b822d0
Raimov Rinat <raimovrinat@yandex.ru>
2017-03-14 22:05:04 +0300

    правки стилей

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,21 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var htmlmin = require("gulp-htmlmin");
+var minify = require("gulp-csso");
+var imagemin = require("gulp-imagemin");
+var rename = require("gulp-rename");
+var del = require("del");
+var run = require("run-sequence");
 var server = require("browser-sync").create();
 
-
+gulp.task("images", function() {
+  return gulp.src("build/img/**/*.{png, jpg,gif}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true})
+      ]))
+    .pipe(gulp.dest("build/img"))
+});
 
 gulp.task("style", function() {
   gulp.src("less/style.less")
@@ -16,22 +28,57 @@ gulp.task("style", function() {
     .pipe(less())
     .pipe(postcss([
       autoprefixer({browsers: [
-        "last 2 versions"
+        "last 1 versions",
+        "last 2 Chrome versions",
+        "last 2 Firefox versions",
+        "last 2 Opera versions",
+        "last 2 Edge versions"
       ]})
     ]))
-    .pipe(gulp.dest("css"))
-    .pipe(server.stream());
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"));
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "fonts/**/*.{woff,woff2}",
+    "img/**",
+    "js/**",
+    "*.html"
+  ], {
+    base: "."
+})
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function() {
+  return del("build");
 });
 
 gulp.task("htmlminify", function(){
-  return gulp.src("index.html")
+  return gulp.src("build/*.html")
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest("build/"));
 });
 
+gulp.task("build", function(fn) {
+  run(
+    "clean",
+    "copy",
+    "style",
+    "images",
+    "htmlminify",
+    fn
+  );
+});
+
+
+
 gulp.task("serve", ["style"], function() {
   server.init({
-    server: ".",
+    server: "build",
     notify: false,
     open: true,
     cors: true,
